91c599bd808d559ef8a8ea4e5e519e20944c8b05
Charlio <chalioxumykj>
2020-11-05 14:27:15 -0800

    added back print list in rec03 tests

--- src/test/java/rec03/InheritanceSortedIntListTest.java
+++ src/test/java/rec03/InheritanceSortedIntListTest.java
@@ -1,4 +1,15 @@
 package rec03;
 
 public class InheritanceSortedIntListTest {
+    /**
+     * A helper function that prints out the contents of an IntegerList.
+     * @param list IntegerList to be printed out.
+     */
+    private void printList(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
 }
