bf9e051f2815cd7ce2534e72c44514af2d13fd13
Charlio <chalioxumykj>
2020-11-05 20:14:07 -0800

    deleted uncleaned rec and hw

--- src/main/java/hw05/core/Processor.java
+++
@@ -1,7 +0,0 @@
-package hw05.core;
-
-import java.util.List;
-
-abstract class Processor {
-    abstract DataSet apply(List<DataSet> sources);
-}
--- src/main/java/hw06/bank/AccountOwner.java
+++
@@ -1,7 +0,0 @@
-package hw06.bank;
-
-/**
- * Represents the owner of a bank account
- */
-public interface AccountOwner {
-}
