2de235c4bbe45ce82bffc25ea0e2a4db9a84e0fd
Charlio <chalioxumykj>
2020-11-12 18:44:05 -0800

    added finished rec07: more gui

---
+++ src/main/java/rec07/gui/TicClient.java
@@ -0,0 +1,15 @@
+package rec07.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import rec07.core.GameChangeListener;
+import rec07.core.TicTacToe;
+
+public class TicClient extends JPanel {
+	private TicTacToe ticTacToe;
+	
+	public TicClient(TicTacToe ticTacToe) {
+
+	}
+}
